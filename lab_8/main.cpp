bef8dd9c24dba7f7767f9daac5840d197aefbebb
MaGmYmS <108451712+MaGmYmS@users.noreply.github.com>
2024-05-11 12:14:13 +0500

    task 3 ready

--- lab_8/main.cpp
+++ lab_8/main.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 #include <omp.h>
 
-void parallel_sum(int N, int num_threads) {
+void parallel_sum_variable(int N, int num_threads) {
     int total_sum = 0;
 
-    #pragma omp parallel if(N > num_threads) num_threads(num_threads) reduction(+:total_sum)
+    #pragma omp parallel if(N > num_threads) num_threads(num_threads) reduction(+:total_sum) // Создаются временные переменные, куда записывается сумма. После, эти переменные складываются с основной переменной
     {
         int id = omp_get_thread_num(); // Получаем номер текущей нити
         int partial_sum = 0;
@@ -14,16 +14,40 @@ void parallel_sum(int N, int num_threads) {
             partial_sum += i;
             std::cout << "[" << id << "]: Sum = " << partial_sum << std::endl;
         }
-    }
 
+        total_sum += partial_sum;
+    }
     std::cout << "Sum = " << total_sum << std::endl;
 }
+
+void parallel_sum_for(int N, int k) {
+    int total_sum = 0;
+
+    #pragma omp parallel num_threads(k)
+    {
+        int id = omp_get_thread_num(); // Получаем номер текущей нити
+        int partial_sum = 0;
+
+        // Распараллеливаем цикл с помощью директивы for
+        #pragma omp for
+        for (int i = 1; i <= N; ++i) {
+            partial_sum += i;
+        }
+        std::cout << "[" << id << "]: Sum = " << partial_sum << std::endl;
+
+        #pragma omp critical
+        total_sum += partial_sum;
+    }
+    std::cout << "Sum = " << total_sum << std::endl;
+}
+
 
 int main() {
     int N = 10;
     int num_threads = 5;
+    parallel_sum_variable(N, num_threads);
 
-    parallel_sum(N, num_threads);
+    parallel_sum_for(N, num_threads);
 
     return 0;
 }
